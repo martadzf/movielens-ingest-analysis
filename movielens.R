ba7d924b4a08830c9fd9b311b0f1edbc24d2ba90
Marta Díaz Fuentes <martadiazfuentes@outlook.es>
2022-11-04 11:40:26 +0100

    Changed .gitignore

---
+++ movielens.R
@@ -0,0 +1,17 @@
+
+# DATA INGESTION ----------------------------------------------------------
+
+## Kaggle API
+
+
+# LOAD LIBS ---------------------------------------------------------------
+
+if(!require("pacman")) install.packages("pacman")
+p_load(tidyverse, magrittr, janitor)
+
+# CLEANING AND FORMATTING -------------------------------------------------
+
+
+# STATS,  EDA, PLAYING AROUND ---------------------------------------------
+
+
