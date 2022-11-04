35188db8948a7cc9f1fbdf93a67e601f0f050971
Marta Díaz Fuentes <martadiazfuentes@outlook.es>
2022-11-04 12:00:14 +0100

    Cleaning, formatting, stats, eda, playing

--- movielens.R
+++ movielens.R
@@ -7,11 +7,19 @@
 # LOAD LIBS ---------------------------------------------------------------
 
 if(!require("pacman")) install.packages("pacman")
-p_load(tidyverse, magrittr, janitor)
+p_load(tidyverse, magrittr, janitor, lubridate)
 
 # CLEANING AND FORMATTING -------------------------------------------------
+m<-read_csv("ml-25m/movies.csv") %>% as_tibble()
+r<-read_csv("ml-25m/ratings.csv") %>% as_tibble()
+t<-read_csv("ml-25m/tags.csv") %>% as_tibble()
+l<-read_csv("ml-25m/links.csv") %>% as_tibble()
 
+# STATS, EDA, PLAYING AROUND ----------------------------------------------
+m %>% glimpse()
+t %>% glimpse()  
 
-# STATS,  EDA, PLAYING AROUND ---------------------------------------------
+t %<>% mutate(timestamp=as_datetime(timestamp)) %>% view()
+r %<>% mutate(timestamp=as_datetime(timestamp)) %>% glimpse()
 
-
+m %>% glimpse()
